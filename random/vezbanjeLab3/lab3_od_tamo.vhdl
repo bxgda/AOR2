4f5ee9d7dd2ac470239025e361938b18abed3779
bxgda <jovan.bogdanovic@elfak.rs>
2024-04-24 21:02:57 +0200

    random popravljeno

--- random/vezbanjeLab3/lab3_od_tamo.vhdl
+++ random/vezbanjeLab3/lab3_od_tamo.vhdl
@@ -2,118 +2,103 @@ library ieee ;
     use ieee.std_logic_1164.all ;
     use ieee.numeric_std.all ;
 
-
-entity andGate is
+entity org is
   port (
     a, b : in std_logic;
-    y : out std_logic;
+    c : out std_logic;
   ) ;
-end andGate ; 
+end org ; 
 
+architecture aorg of org   is
 
-architecture a_andGate of andGate is
 begin
-    y <= a and b;
-end architecture ;
----------------------------------------- end a_andGate
+    c <= a or b;
 
+end architecture ;
+
+---------------------------------------------- end org
 
 library ieee ;
     use ieee.std_logic_1164.all ;
     use ieee.numeric_std.all ;
 
-
-entity orGate is
+entity andg is
   port (
     a, b : in std_logic;
-    y : out std_logic;
+    c : out std_logic;
   ) ;
-end orGate ; 
+end andg ; 
 
+architecture aandg of andg is
 
-architecture a_orGate of orGate is
 begin
-    y <= a or b;
-end architecture ;
----------------------------------------- end a_orGate
+    c <= a and b;
 
+end architecture ;
+
+----------------------------------------------- end andg
 
 library ieee ;
     use ieee.std_logic_1164.all ;
     use ieee.numeric_std.all ;
-
 
 entity kolo is
-    generic (n : positive := 4);
-    port (
-        a, b : in std_logic_vector(n-1 downto 0);
-        c : in std_logic;
-        y : out std_logic_vector(n-1 downto 0);
-    ) ;
+    generic(n:integer := 4);
+  port (
+    a, b : in std_logic_vector(n-1 downto 0);
+    ulaz1 : in std_logic;
+    c : out std_logic_vector(n-1 downto 0);
+  ) ;
 end kolo ; 
 
+architecture akolo of kolo is
+    signal medjunula : std_logic;
 
-architecture a_kolo of kolo is
-    signal y_int : std_logic_vector(n-1 downto 0);
 begin
+    nultiand : entity work.andg(aandg)
+        port map(a(0), b(0), medjunula);
+    nultior : entity work.org(aorg)
+        port map(medjunula, ulaz1, c(0));
+    
+    generisi : for i in 1 to n-1 generate
+        signal medju : std_logic;
 
-
-    -- mora nulti iznad jer kaze iskljucivo for generate
-    nulti_andKomp: entity work.andGate(a_andGate)
-    port map (a(0), b(0), y_int(0));
-
-
-    nulti_orKomp: entity work.orGate(a_orGate)
-        port map(y_int(0), c, y(0));
-
-    generisi: for i in 1 to n-1 generate
     begin
-        andKomp: entity work.andGate(a_andGate)
-            port map (a(i), b(i), y_int(i));
+        itiand : entity work.andg(aandg)
+            port map (a(i), b(i), medju);
+        itior : entity work.org(aorg)
+            port map (medju, c(i-1), c(i));
 
-
-        orKomp: entity work.orGate(a_orGate)
-            port map(y_int(i), y(i-1), y(i));
-    end generate generisi;
+    end generate;
 end architecture ;
 
-
-------------------------------------------- end a_kolo
-
-
--- Testbench
-
+----------------------------------------------- end kolo
 
 library ieee ;
     use ieee.std_logic_1164.all ;
     use ieee.numeric_std.all ;
 
-
 entity tb is
-  generic (n : positive := 4);
+    generic(n:integer := 4);
 end tb ; 
 
+architecture atb of tb is
+    signal ulaz1 : std_logic;
+    signal a, b, c : std_logic_vector(n-1 downto 0);
 
-architecture a_tb of tb is
-    signal a, b, y : std_logic_vector (n-1 downto 0);
-    signal c : std_logic;
 begin
+    dut : entity work.kolo(akolo)
+        port map(a,b,ulaz1,c);
 
+    stimuli : process
 
-    dut: entity work.kolo(a_kolo)
-        port map (a, b, c, y);
-
-
-    stimuli: process
     begin
-        a <= "1101";
-        b <= "1001";
-        c <= '1';
-        wait for 100 ns;
-    end process stimuli;
+        a <= "0101";
+        b <= "1010";
+        ulaz1 <= '1';
+        wait for 50 ns;
 
-
+    end process;
 end architecture ;
 
-
--------------------------------------------- end testbench
+--------------------------------------------- end tb
